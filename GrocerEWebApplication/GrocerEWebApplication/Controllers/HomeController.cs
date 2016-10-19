84b8804bca044f99582c9b4e48165327dfa41d8b
Zachary de Boer <deboerz@sheridancollege.ca>
2016-10-19 16:37:03 -0400

    Started implementing test persistence. Slight modifications of many classes.

--- GrocerEWebApplication/GrocerEWebApplication/Controllers/HomeController.cs
+++ GrocerEWebApplication/GrocerEWebApplication/Controllers/HomeController.cs
@@ -13,5 +13,9 @@ namespace GrocerEWebApplication.Controllers
         {
             return View();
         }
+
+
+
+       
     }
 }
--- GrocerEWebApplication/GrocerEWebApplication/Models/Entities/Item.cs
+++ GrocerEWebApplication/GrocerEWebApplication/Models/Entities/Item.cs
@@ -7,5 +7,8 @@ namespace GrocerEWebApplication.Models
 {
     public class Item
     {
+
+
+
     }
 }
