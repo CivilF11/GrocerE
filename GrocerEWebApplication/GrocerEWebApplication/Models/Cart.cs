d720dfc46f6f0a8b4f210e9adca9e67249abd517
Zachary de Boer <deboerz@sheridancollege.ca>
2016-10-19 14:57:56 -0400

    Implemented Cart class.

--- GrocerEWebApplication/GrocerEWebApplication/Models/Cart.cs
+++ GrocerEWebApplication/GrocerEWebApplication/Models/Cart.cs
@@ -7,6 +7,45 @@ namespace GrocerEWebApplication.Models
 {
     public class Cart
     {
+        private List<Order> _contents;
 
+        private Double _subTotal;
+
+
+
+        public List<Order> Contents
+        {
+            get
+            {
+                return _contents;
+            }
+
+            set
+            {
+                _contents = value;
+            }
+        }
+
+
+
+        public Double SubTotal
+        {
+            get
+            {
+                //todo implement
+                return _subTotal;
+            }
+        }
+
+
+
+
+        public Cart()
+        {
+
+            Contents = new List<Order>();
+
+        }
+        
     }
 }
